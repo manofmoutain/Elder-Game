487de0dd1bd398bb8027d6e207eb8395a03c6234
manofmoutain <m97532007@gmail.com>
2022-07-05 09:56:47 +0800

    Add Vietnam database SO

---
+++ Assets/Resource/Asia/ModelC/越南/Scripts/Database/ModelCVietnamDatabase.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Global.Database;
+using UnityEngine;
+
+namespace Asia.Vietnam.Database
+{
+    [CreateAssetMenu(fileName = "亞洲-越南-", menuName = "亞洲/越南", order = 6)]
+    public class ModelCVietnamDatabase : ModelCNationDatabase
+    {
+
+    }
+}
+
