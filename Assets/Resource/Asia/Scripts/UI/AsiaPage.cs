e2cf0a772abae1edb97b7320f35d35aad6c60d39
manofmoutain <m97532007@gmail.com>
2022-07-08 15:45:00 +0800

    edit asiapage.cs button's load scene

--- Assets/Resource/Asia/Scripts/UI/AsiaPage.cs
+++ Assets/Resource/Asia/Scripts/UI/AsiaPage.cs
@@ -22,12 +22,12 @@ namespace Asia.UI
         {
             LoadScenes = new SceneLoader();
 
-            chinaBtn.onClick.AddListener(delegate { LoadScenes.LoadScene(8); });
-            philipineBtn.onClick.AddListener(delegate { LoadScenes.LoadScene(9); });
-            japanBtn.onClick.AddListener(delegate { LoadScenes.LoadScene(10); });
-            thailandBtn.onClick.AddListener(delegate { LoadScenes.LoadScene(11); });
-            taiwanBtn.onClick.AddListener(delegate { LoadScenes.LoadScene(12); });
-            vietnamBtn.onClick.AddListener(delegate { LoadScenes.LoadScene(13); });
+            chinaBtn.onClick.AddListener(delegate { LoadScenes.LoadScene(7); });
+            philipineBtn.onClick.AddListener(delegate { LoadScenes.LoadScene(8); });
+            japanBtn.onClick.AddListener(delegate { LoadScenes.LoadScene(9); });
+            thailandBtn.onClick.AddListener(delegate { LoadScenes.LoadScene(10); });
+            taiwanBtn.onClick.AddListener(delegate { LoadScenes.LoadScene(11); });
+            vietnamBtn.onClick.AddListener(delegate { LoadScenes.LoadScene(12); });
         }
     }
 }
