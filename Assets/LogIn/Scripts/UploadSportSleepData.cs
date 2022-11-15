991e916f5d5ad0aee0ba4542e3a0c7135cb9faa8
PeiRong <s1311034016@gms.nutc.edu.tw>
2022-11-15 19:41:12 +0800

    upload toast

--- Assets/LogIn/Scripts/UploadSportSleepData.cs
+++ Assets/LogIn/Scripts/UploadSportSleepData.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using UnityEngine;
+using System.Text.RegularExpressions;
 
 public class UploadSportSleepData : MonoBehaviour
 {
     string url = "http://ring.nutc.edu.tw/garmin/Joyce/upload_SportSleepData.php";
     private string account;
     private string password;
+    public bool upload;
+
 
     void Start()
     {
+        if(upload){
+            StartCoroutine(UploadSportSleepDatas());
+        }
+        
+        Debug.Log("upload");
+    }
+    public void click(){
         StartCoroutine(UploadSportSleepDatas());
     }
-    
     IEnumerator UploadSportSleepDatas()
     {
         account=SignIn.account;
@@ -23,10 +32,18 @@ public class UploadSportSleepData : MonoBehaviour
         form.AddField("account", account);
         form.AddField("password", password);
         WWW www = new WWW(url, form);
+        
 
         yield return www;
+        if(string.IsNullOrEmpty(www.error)){
+            Debug.Log(www.text);
+            var received_data = Regex.Split(www.text, "</next>");
+            Debug.Log(received_data[(received_data.Length)-1]);
+            GetComponent<Toast>().showToast(received_data[(received_data.Length)-1], 5);
+        }
         
     }
+
 }
 
 
